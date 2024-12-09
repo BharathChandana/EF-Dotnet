76401ed9a5739caa3efb9a8b34f63d670475ad57
BharathChandana <bharathchandana43@gmail.com>
2024-12-09 11:22:50 +0530

    Added code

---
+++ EfOne1/EfOne1/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfOne1
+{
+    internal class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int EmployeeId {  get; set; }
+
+        [Required]
+        [MaxLength (50)]
+        public string EmployeeName {  get; set; }
+
+        [Required]
+        [MaxLength (50)]
+        public string EmployeeRole {  get; set; }
+
+       
+        public double EmployeeSalary { get; set; }
+
+        [Required]
+        [MaxLength (20)]
+        public string EmployeeGender {  get; set; }
+    }
+}
---
+++ EfOne1/EfOne1/EmployeeContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfOne1
+{
+    internal class EmployeeContext:DbContext
+    {
+        public EmployeeContext():base()
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+        }
+        public DbSet<Employee> employees { get; set; }
+    }
+}
